b08358458fa4fabc9ba54c93df5e469b02a38f4c
sergeyomel__ <72709764+sergeyomel@users.noreply.github.com>
2022-06-20 22:08:46 +0300

    Add files via upload

---
+++ GraphicsGraph.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GraphLib;
+using Graphics.Connectors;
+using System.Windows.Controls;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Media;
+
+namespace Graphics
+{
+    public class GraphicsGraph
+    {
+        private Canvas canvas;
+        private int radiusVertex = 25;
+
+        public GraphicsGraph(Canvas c)
+        {
+            canvas = c;
+            graph = new Graph();
+        }
+
+        public Graph graph;
+        public List<GraphicsVertex> lGraphVertex = new List<GraphicsVertex>();
+        public List<GraphicsVertexConnector> lGraphicsVertexConnectors = new List<GraphicsVertexConnector>();
+
+        public GraphicsVertex ContainVertex(double x, double y, int length = 60)
+        {
+            var nx = x - radiusVertex;
+            var ny = y - radiusVertex;
+            foreach (var item in lGraphVertex)
+                if (Math.Sqrt(Math.Pow((nx - item.posX), 2) + Math.Pow((ny - item.posY), 2)) <= length)
+                    return item;
+            return null;
+        }
+        public GraphicsVertex AddGraphVertex(double x, double y)
+        {
+            var nx = x - radiusVertex;
+            var ny = y - radiusVertex;
+            var vertex = graph.AddVertex();
+            lGraphVertex.Add(new GraphicsVertex(vertex, nx, ny));
+            Draw();
+            return lGraphVertex.Last();
+        }
+        public void RemoveGraphVertex(double x, double y)
+        {
+            var vertex = ContainVertex(x, y, radiusVertex - 1);
+            if (vertex != null)
+            {
+                var lGV = new List<GraphicsVertexConnector>(lGraphicsVertexConnectors);
+                foreach (var connector in lGraphicsVertexConnectors)
+                    if (connector.startVertex.vertex.Equal(vertex.vertex) ||
+                        connector.endVertex.vertex.Equal(vertex.vertex))
+                        lGV.Remove(connector);
+                lGraphicsVertexConnectors = lGV;
+
+                lGraphVertex.Remove(vertex);
+                graph.RemoveVertex(vertex.vertex);
+
+                Draw();
+            }
+        }
+
+        private GraphicsVertexConnector FindGraphVertConnector(double x, double y)
+        {
+            foreach(var connector in lGraphicsVertexConnectors)
+            {
+                (double x, double y) p1 = (connector.startVertex.posX, connector.startVertex.posY);
+                (double x, double y) p2 = (connector.endVertex.posX, connector.endVertex.posY);
+                (double x, double y) p3 = (x, y);
+
+                if ((1 / 2) * Math.Abs((p2.x - p1.x) * (p3.y - p1.y) - (p3.x - p1.x) * (p2.y - p1.y)) < 2)
+                    return connector;
+            }
+            return null;
+        }
+        public void AddVertConnector(GraphicsVertex startVert, GraphicsVertex endVert)
+        {
+            var resAddVertConnector = graph.AddVConnector(startVert.vertex, endVert.vertex);
+            if(resAddVertConnector != null)
+            {
+                if (graph.GetTypeGraph() == GraphLib.TypeGraph.Undirected)
+                    lGraphicsVertexConnectors.Add(new GraphicsEdge(resAddVertConnector, startVert, endVert));
+                else
+                    lGraphicsVertexConnectors.Add(new GraphicsArc(resAddVertConnector, startVert, endVert));
+                Draw();
+            }
+        }
+        public void RemoveVertConnector(double x, double y)
+        {
+            var resContainVC = FindGraphVertConnector(x, y);
+            if (resContainVC == null)
+                return;
+            lGraphicsVertexConnectors.Remove(resContainVC);
+            graph.RemoveVConnector(resContainVC.vertConnector);
+            Draw();
+        }
+
+        public void ChangeTypeGraphicsGraph()
+        {
+            graph.ChangeTypeGraph();
+            var buffGraphVertConnector = new List<GraphicsVertexConnector>();
+            foreach (var connector in lGraphicsVertexConnectors)
+                buffGraphVertConnector.Add(GraphicsVertexConnector.ConvertConnector(connector));
+            lGraphicsVertexConnectors = buffGraphVertConnector;
+            Draw();
+        }
+
+        public void Draw()
+        {
+            canvas.Children.Clear();
+            foreach (var vertex in lGraphVertex)
+            {
+                var tupleDraw = vertex.Draw();
+                canvas.Children.Add(tupleDraw.Item1);
+                canvas.Children.Add(tupleDraw.Item2);
+            }
+            foreach (var connector in lGraphicsVertexConnectors)
+            {
+                foreach (var item in connector.Draw())
+                    canvas.Children.Add(item);
+            }
+
+            var Writer = new GraphLib.Writer.WriterFromFile(@"D:\Input.txt");
+            Writer.WriteAdjancyMatrix(graph);
+        }
+
+        public void ColoringGraph(Genetic.Individual individ)
+        {
+            var dColorVert = individ.GetDColorVertex();
+            var sColor = new HashSet<Color>();
+            var lColor = ColorSet.GetSetColor(dColorVert.GroupBy(item => item.Value).Count()); //Цвета не меняются, потому что генерятся в количестве вершин графа.
+            foreach (var gVert in lGraphVertex)
+            {
+                foreach (var vert in dColorVert.Keys.ToList())
+                {
+                    if (gVert.vertex.Equal(vert))
+                    {
+                        gVert.SetFillColor(lColor[dColorVert[vert]]);
+                        if (!sColor.Contains(lColor[dColorVert[vert]]))
+                            sColor.Add(lColor[dColorVert[vert]]);
+                    }
+                        
+                }
+            }
+            Draw();
+        }
+
+        public void ReserGraphicsVertColor()
+        {
+            foreach (var gVert in lGraphVertex)
+                gVert.SetFillColor(Color.FromRgb(35, 255, 35));
+            Draw();
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,320 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using GraphLib;
+using Genetic;
+using Genetic.Generations.GenerationInterface;
+using Genetic.Crossover;
+using System.Diagnostics;
+using GraphLib.Writer;
+using Genetic.Generations;
+using Genetic.Selection;
+using Genetic.Mutation;
+using Genetic.PowerFunction;
+using Genetic.JSON.HistogramConstructionModel;
+using Genetic.JSON.SetBuiltDependencies;
+using System;
+using System.Collections.Generic;
+using Genetic.JSON;
+
+namespace Graphics
+{
+    public partial class MainWindow : Window
+    {
+        private GraphicsGraph graphicsGraph;
+
+        private Genetic.Interface.ICrossing crossover;
+        private Genetic.Interface.IMutation mutation;
+        private Genetic.Interface.ISelect selector;
+        private Genetic.Interface.IPowerFunction powerFunction;
+
+        private bool IsCheckVert = false;
+        private GraphicsVertex bufferGrVert = null;
+        private int StrokeTh = 2;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Title = "РаскрасьМеняПолностью";
+
+            crossover = new SinglePointCrossing();
+            mutation = new DoublePointMutation();
+            selector = new MinimumTargetFunctionSelection();
+            powerFunction = new ColoringNonAdjacentVertices();
+
+            
+
+            graphicsGraph = new GraphicsGraph(CanvasPanel);
+            Logger.InitializeLogger();
+        }
+
+        private void GenerateColoringGraph(Graph graph, Population population, Individual individ, int count = 0, string title = "example")
+        {
+            var window = new Window();
+            window.Title = title;
+            window.Height = 850;
+            window.Width = 1280;
+            window.Background = new SolidColorBrush(Color.FromRgb(220, 220, 220));
+            var canvas = new Canvas();
+            canvas.Height = 850;
+            canvas.Width = 1280;
+            canvas.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            window.Content = canvas;
+
+            var converter = new ConverterGraphToGraphicsGraph(canvas);
+            var graphicsGraph2 = converter.ConvertGraph(graph);
+            graphicsGraph2.ColoringGraph(individ);
+
+            var tbC = new TextBlock();
+            tbC.Text = "Colors: "+count.ToString();
+            tbC.FontSize = 18;
+            tbC.FontWeight = FontWeights.Bold;
+            Canvas.SetLeft(tbC, canvas.Width - count.ToString().Length * 20 - 80);
+            Canvas.SetTop(tbC, 25);
+            canvas.Children.Add(tbC);
+
+            window.Show();
+
+            SaverToPicture.Save(window, @"C:\Users\myacc\Desktop\pictureGraph");
+            //SaverInfoAboutPopulation.Save(population);
+            
+        }
+
+        private void ClickLeftButton(object sender, MouseButtonEventArgs e)
+        {
+            Point p = e.GetPosition(CanvasPanel);
+            var graphVert = graphicsGraph.ContainVertex(p.X, p.Y);
+
+            if (graphVert == null)
+            {
+                if (IsCheckVert)
+                {
+                    bufferGrVert.SetStrokeTh(StrokeTh);
+                    graphicsGraph.Draw();
+                    IsCheckVert = false;
+                    bufferGrVert = null;
+                }
+                else
+                    graphicsGraph.AddGraphVertex(p.X, p.Y);
+            }
+            else
+            {
+                if (IsCheckVert)
+                {
+                    bufferGrVert.SetStrokeTh(StrokeTh);
+                    graphicsGraph.AddVertConnector(bufferGrVert, graphVert);
+                    IsCheckVert = false;
+                    bufferGrVert = null;
+                }
+                else
+                {
+                    IsCheckVert = true;
+                    bufferGrVert = graphVert;
+                    StrokeTh = bufferGrVert.GetStrokeTh();
+                    bufferGrVert.SetStrokeTh(4);
+                    graphicsGraph.Draw();
+                }
+            }
+        }
+
+        private void ClickRightButton(object sender, MouseButtonEventArgs e)
+        {
+            Point p = e.GetPosition(this);
+            graphicsGraph.RemoveGraphVertex(p.X, p.Y);
+        }
+
+        private void ColoringCurrentGraph(object sender, RoutedEventArgs e)
+        {
+            if (!GeneratorGraph.IsConnectedGraph(graphicsGraph.graph))
+            {
+                MessageBox.Show("Граф не является полносвязным.");
+                return;
+            }
+            var population = new Population(100, 100, 100, 0, 100, graphicsGraph.graph, new FillingRandomIndividuals(), crossover, mutation, selector, powerFunction);
+            population.FindBestIndivid();
+            graphicsGraph.ColoringGraph(population.GetBestIndividInPopulation());
+        }
+        private void ColoringFileGraph(object sender, RoutedEventArgs e)
+        {
+            /*var reader = new ReaderFromFile();
+            var strAdjMatrix = reader.Read(@"C:\Users\myacc\OneDrive\Рабочий стол\ChromaGraph\ExampleGraphThreeColor3.txt");
+            var graph = ConverterStringAdjMatrix.GetGraph(strAdjMatrix);*/
+
+            var reader = new GraphLib.Reader.ReaderFromFile();
+            var strmatrix = reader.Read(@"C:\Users\myacc\OneDrive\Рабочий стол\matrix (2).txt");
+            var graph = ConverterStringAdjMatrix.GetGraph(strmatrix);
+
+            var population = new Population(100, 100, 100, 0, 100, graphicsGraph.graph, new FillingRandomIndividuals(), crossover, mutation, selector, powerFunction);
+            population.FindBestIndivid();
+            
+            GenerateColoringGraph(graph, population, population.GetBestIndividInPopulation(), population.GetBestIndividInPopulation().TargetFunction());
+        }
+        private void ColoringRandomGraph(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                var randomGraph = GeneratorGraph.Generate(50, 150);
+
+                var population = new Population(
+                    100,
+                    50,
+                    1000,
+                    5,
+                    100,
+                    randomGraph,
+                    new FillingRandomIndividuals(),
+                    new AccordingToEvalutionMatrixCrossover(),
+                    new DoublePointMutation(),
+                    new MinimumTargetFunctionSelection(),
+                    new NonDecreasingDegrees());
+                population.FindBestIndivid();
+                JSONWriter.Write(population);
+
+                population = new Population(
+                    100,
+                    50,
+                    1000,
+                    750,
+                    100,
+                    randomGraph,
+                    new FillingEliteIndividuals(),
+                    new AccordingToEvalutionMatrixCrossover(),
+                    new DoublePointMutation(),
+                    new MinimumTargetFunctionSelection(),
+                    new VozikaAlgorithm());
+                population.FindBestIndivid();
+                JSONWriter.Write(population);
+
+                //GenerateColoringGraph(randomGraph, population, population.GetBestIndividInPopulation(), population.GetBestIndividInPopulation().TargetFunction(), "RoadCrossover");
+                //var writer = new WriterFromFile(@"C:\Users\myacc\Desktop\graphColor.txt");
+                //writer.WriteColorGraph(randomGraph, new Genetic.PowerFunction.NonDecreasingDegrees().IndividualPowerFunction(population.GetBestIndividInPopulation()));
+            }
+        }
+
+        private void ResetColor(object sender, RoutedEventArgs e)
+        {
+            graphicsGraph.ReserGraphicsVertColor();
+        }
+
+        private void ChangeType(object sender, RoutedEventArgs e)
+        {
+            graphicsGraph.ChangeTypeGraphicsGraph();
+        }
+
+        private void DrawHistogram(object sender, RoutedEventArgs e)
+        {
+            #region Histogram
+
+            DrawHistogram9(null, null);
+            //DrawHistogram10(null, null);
+
+            /*Graphics.SaverToPicture.Save(histogram.GetWindow(), @"C:\Users\myacc\OneDrive\Рабочий стол\GraphsPicture");*/
+            #endregion
+        }
+
+        private void DrawHistogram9(object sender, RoutedEventArgs e)
+        {
+            #region Histogram
+            var requestModel = new RequestModel(DependentElement.ChromaticNumber,
+                                                RequestParameter.FillingType,
+                                                ParameterGroupColumn.CountIndividInGeneration);
+            var signature = new ParameterToStringConverter().GetStringParameter(requestModel);
+            var histogram = new Histogram.Histogram();
+            var pattern = new Pattern();
+
+            List<ModelJSON> data;
+            try
+            {
+                data = JSONReader.Read();
+
+                foreach (var item in pattern.GetData(data, requestModel))
+                    histogram.AddGroup(item);
+            }
+            catch (Exception ex)
+            {
+                Logger.PushMessage(ex);
+                MessageBox.Show(ex.Message);
+                return;
+            };
+
+            var lColumnName = pattern.signatureColumn;
+            histogram.SetColumnName(new PropertyToStringConverter().GetStringParameter(lColumnName));
+
+            histogram.SetTitleHistogram($"Зависимость {signature[0]} от {signature[1]}");
+            histogram.SetSignatureOrdinat(signature[0]);
+            histogram.SetSignatureHistogram(signature[2]);
+
+            histogram.DrawHistogram();
+
+            /*Graphics.SaverToPicture.Save(histogram.GetWindow(), @"C:\Users\myacc\OneDrive\Рабочий стол\GraphsPicture");*/
+            #endregion
+        }
+
+        private void DrawHistogram10(object sender, RoutedEventArgs e)
+        {
+            #region Histogram
+            var requestModel = new RequestModel(DependentElement.ChromaticNumber,
+                                                RequestParameter.ProbabilityMutation,
+                                                ParameterGroupColumn.CountIndividInGeneration);
+            var signature = new ParameterToStringConverter().GetStringParameter(requestModel);
+            var histogram = new Histogram.Histogram();
+            var pattern = new Pattern();
+
+            List<ModelJSON> data;
+            try
+            {
+                data = JSONReader.Read();
+
+                foreach (var item in pattern.GetData(data, requestModel))
+                    histogram.AddGroup(item);
+            }
+            catch (Exception ex)
+            {
+                Logger.PushMessage(ex);
+                MessageBox.Show(ex.Message);
+                return;
+            };
+
+            var lColumnName = pattern.signatureColumn;
+            histogram.SetColumnName(lColumnName);
+
+            histogram.SetTitleHistogram($"Зависимость {signature[0]} от {signature[1]}");
+            histogram.SetSignatureOrdinat(signature[0]);
+            histogram.SetSignatureHistogram(signature[2]);
+
+            histogram.DrawHistogram();
+
+            /*Graphics.SaverToPicture.Save(histogram.GetWindow(), @"C:\Users\myacc\OneDrive\Рабочий стол\GraphsPicture");*/
+            #endregion
+        }
+
+        private void DrawCoordinateLine()
+        {
+            #region CoordinateLine
+            var widthRegion = 600;
+            var heightRegion = 600;
+
+            var lineX = new System.Windows.Shapes.Line();
+            lineX.X1 = 0;
+            lineX.X2 = widthRegion;
+            lineX.Y1 = heightRegion / 2;
+            lineX.Y2 = heightRegion / 2;
+            lineX.Stroke = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+            lineX.StrokeThickness = 2;
+            Canvas.SetZIndex(lineX, 1);
+            CanvasPanel.Children.Add(lineX);
+
+            var lineY = new System.Windows.Shapes.Line();
+            lineY.X1 = widthRegion / 2;
+            lineY.X2 = widthRegion / 2;
+            lineY.Y1 = 0;
+            lineY.Y2 = heightRegion;
+            lineY.Stroke = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+            lineY.StrokeThickness = 2;
+            Canvas.SetZIndex(lineY, 1);
+            CanvasPanel.Children.Add(lineY);
+            #endregion
+        }
+    }
+}
